376d9f5aeff20e8e4058dbf25fa90a6b5c7a8371
LUTlp <lutlp@outlook.com>
2018-06-29 11:50:41 +0200

    avecJavaScript

---
+++ main.js
@@ -0,0 +1,17 @@
+setTimeout(function (){
+  alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')
+}, 10000);
+
+//bouton psychédélique
+/*  var mapage = document.querySelector('html');
+  var monbouton= document.querySelector('button')
+
+function changerlacouleur (){
+
+  var ma couleur = document.querySelector ('html')
+  if ;
+
+}
+*/
+document.querySelector('button').onclick= function (){
+  backgroundColor = body.magenta};
